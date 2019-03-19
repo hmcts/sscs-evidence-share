c7ef4a28a68bf0da736a54ee6720f54db9ee14c1
jackmaloney <jack.maloney@transformuk.com>
2019-03-19 13:22:49 +0000

    SSCS-5318 Populate placeholders for DL6

--- src/test/java/uk/gov/hmcts/reform/sscs/service/DocumentPlaceholderServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/DocumentPlaceholderServiceTest.java
@@ -5,10 +5,10 @@ import static org.junit.Assert.assertEquals;
 import static uk.gov.hmcts.reform.sscs.config.PlaceholderConstants.*;
 
 import java.util.Map;
+import org.joda.time.DateTimeUtils;
 import org.junit.Before;
 import org.junit.Test;
-import uk.gov.hmcts.reform.sscs.ccd.domain.RegionalProcessingCenter;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
 
 public class DocumentPlaceholderServiceTest {
 
@@ -20,10 +20,12 @@ public class DocumentPlaceholderServiceTest {
     private static final String POSTCODE = "L2 5UZ";
 
     private DocumentPlaceholderService service;
+    private SscsCaseData caseData;
 
     @Before
     public void setup() {
         service = new DocumentPlaceholderService();
+        DateTimeUtils.setCurrentMillisFixed(1550000000000L);
     }
 
     @Test
@@ -31,31 +33,57 @@ public class DocumentPlaceholderServiceTest {
         RegionalProcessingCenter rpc = RegionalProcessingCenter.builder()
             .name("Liverpool").address1(RPC_ADDRESS1).address2(RPC_ADDRESS2).address3(RPC_ADDRESS3).address4(RPC_ADDRESS4).city(RPC_CITY).postcode(POSTCODE).build();
 
-        SscsCaseData caseData = SscsCaseData.builder().caseCreated("01/01/2019").regionalProcessingCenter(rpc).build();
+        buildCaseData(rpc);
 
         Map<String, Object> result = service.generatePlaceholders(caseData);
 
-        assertEquals(result.get(CASE_CREATED_DATE_LITERAL), "01/01/2019");
+        assertEquals(result.get(CASE_CREATED_DATE_LITERAL), "2018-10-23");
         assertEquals(result.get(REGIONAL_OFFICE_ADDRESS_LINE1_LITERAL), RPC_ADDRESS1);
         assertEquals(result.get(REGIONAL_OFFICE_ADDRESS_LINE2_LITERAL), RPC_ADDRESS2);
         assertEquals(result.get(REGIONAL_OFFICE_ADDRESS_LINE3_LITERAL), RPC_ADDRESS3);
         assertEquals(result.get(REGIONAL_OFFICE_ADDRESS_LINE4_LITERAL), RPC_ADDRESS4);
         assertEquals(result.get(REGIONAL_OFFICE_COUNTY_LITERAL), RPC_CITY);
         assertEquals(result.get(REGIONAL_OFFICE_POSTCODE_LITERAL), POSTCODE);
+        assertEquals(result.get(DWP_ADDRESS_LINE1_LITERAL), "Dummy dwp address line1");
+        assertEquals(result.get(DWP_ADDRESS_LINE2_LITERAL), "Dummy dwp address line2");
+        assertEquals(result.get(DWP_ADDRESS_LINE3_LITERAL), "Dummy dwp address line3");
+        assertEquals(result.get(DWP_ADDRESS_LINE4_LITERAL), "Dummy dwp address line4");
+        assertEquals(result.get(GENERATED_DATE_LITERAL), "2019-02-12");
+        assertEquals(result.get(APPELLANT_FULL_NAME_LITERAL), "Mr T Tibbs");
+        assertEquals(result.get(BENEFIT_TYPE_LITERAL), "PERSONAL INDEPENDENCE PAYMENT");
+        assertEquals(result.get(CASE_ID_LITERAL), "123456");
+        assertEquals(result.get(NINO_LITERAL), "JT0123456B");
+        assertEquals(result.get(SSCS_URL_LITERAL), "http://www.tribunals.gov.uk/");
     }
 
     @Test
     public void givenACaseDataWithNoRpc_thenGenerateThePlaceholderMappingsWithoutRpc() {
-        SscsCaseData caseData = SscsCaseData.builder().caseCreated("01/01/2019").build();
+        buildCaseData(null);
 
         Map<String, Object> result = service.generatePlaceholders(caseData);
 
-        assertEquals(result.get(CASE_CREATED_DATE_LITERAL), "01/01/2019");
+        assertEquals(result.get(CASE_CREATED_DATE_LITERAL), "2018-10-23");
         assertNull(result.get(REGIONAL_OFFICE_ADDRESS_LINE1_LITERAL));
         assertNull(result.get(REGIONAL_OFFICE_ADDRESS_LINE2_LITERAL));
         assertNull(result.get(REGIONAL_OFFICE_ADDRESS_LINE3_LITERAL));
         assertNull(result.get(REGIONAL_OFFICE_ADDRESS_LINE4_LITERAL));
         assertNull(result.get(REGIONAL_OFFICE_COUNTY_LITERAL));
         assertNull(result.get(REGIONAL_OFFICE_POSTCODE_LITERAL));
+
+    }
+
+    private void buildCaseData(RegionalProcessingCenter rpc) {
+        caseData = SscsCaseData.builder()
+            .caseCreated("2018-10-23T15:04:48.187")
+            .ccdCaseId("123456")
+            .regionalProcessingCenter(rpc)
+            .appeal(Appeal.builder()
+                .benefitType(BenefitType.builder().code("PIP").description("Personal Independence Payment").build())
+                .appellant(Appellant.builder()
+                    .name(Name.builder().title("Mr").firstName("Terry").lastName("Tibbs").build())
+                    .identity(Identity.builder().nino("JT0123456B").build())
+                    .build())
+                .build())
+            .build();
     }
 }
