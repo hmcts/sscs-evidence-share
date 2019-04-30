2587fa475aafbc3d8c1535d8c5e814b720b36f50
Chris Moreton <christopher.moreton@hmcts.net>
2019-04-30 12:53:27 +0100

    Bump azurerm version to 1.22.1

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -13,7 +13,3 @@ output "pdf_service_access_key" {
 output "pdf_service_health_url" {
   value = "${data.azurerm_key_vault_secret.pdf_service_base_url.value}rs/health"
 }
-
-output "consulDnsIp" {
-  value = "${data.azurerm_lb.consul_dns.private_ip_address}"
-}
