58f71664f89d348af3b84a3460c05363d4dd5bc8
david.crespo <david.crespo@hmcts.net>
2019-06-10 13:05:13 +0100

    fix checkstyle issues

--- src/test/java/uk/gov/hmcts/reform/sscs/service/EvidenceShareServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/EvidenceShareServiceTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.then;
