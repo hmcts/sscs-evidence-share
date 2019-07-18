e905ce6aee509b7604b116537670a4a90d05856e
david.crespo <david.crespo@hmcts.net>
2019-07-18 20:27:06 +0200

    refactor production code to move logic to validate callback to separate method

--- src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/IssueFurtherEvidenceHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/IssueFurtherEvidenceHandler.java
@@ -1,18 +1,24 @@
 package uk.gov.hmcts.reform.sscs.callback.handlers;
 
+import java.util.List;
 import uk.gov.hmcts.reform.sscs.callback.CallbackHandler;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocument;
 
 public class IssueFurtherEvidenceHandler implements CallbackHandler<SscsCaseData> {
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback, DispatchPriority priority) {
 
-        return callback.getCaseDetails().getCaseData().getSscsDocument().get(0).getValue().getEvidenceIssued().equals("No")
-            && callback.getCaseDetails().getCaseData().getSscsDocument().get(0).getValue().getDocumentType().equals(DocumentType.APPELLANT_EVIDENCE.getValue());
+        return validateCallback(callback.getCaseDetails().getCaseData().getSscsDocument());
+    }
+
+    private boolean validateCallback(List<SscsDocument> sscsDocument) {
+        return sscsDocument.get(0).getValue().getEvidenceIssued().equals("No")
+            && sscsDocument.get(0).getValue().getDocumentType().equals(DocumentType.APPELLANT_EVIDENCE.getValue());
     }
 
     @Override
