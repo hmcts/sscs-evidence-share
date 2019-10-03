49b3de60d612c4f1042b546a6732863ddc6b01f1
Keith Marshall <keith.marshall@hmcts.net>
2019-10-03 09:58:44 +0100

    rename class to meet checkstyle rules

--- src/main/java/uk/gov/hmcts/reform/sscs/config/SmtpConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/SmtpConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-public class SMTPConfig {
+public class SmtpConfig {
 
     @Value("${appeal.email.host}")
     private String emailHost;
