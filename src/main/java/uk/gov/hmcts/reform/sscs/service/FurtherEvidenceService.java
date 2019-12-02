ef6a1114548f86744920d5f88f8775435645c580
david.crespo <david.crespo@hmcts.net>
2019-12-02 15:19:11 +0000

    fix checkstyle issues

--- src/main/java/uk/gov/hmcts/reform/sscs/service/FurtherEvidenceService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/FurtherEvidenceService.java
@@ -9,7 +9,6 @@ import static uk.gov.hmcts.reform.sscs.domain.FurtherEvidenceLetterType.REPRESEN
 
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
--- src/test/java/uk/gov/hmcts/reform/sscs/callback/handlers/HandlerHelper.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/callback/handlers/HandlerHelper.java
@@ -2,16 +2,16 @@ package uk.gov.hmcts.reform.sscs.callback.handlers;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.State;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class HandlerHelper {
+
+    private HandlerHelper() {
+    }
 
     public static Callback<SscsCaseData> buildTestCallbackForGivenData(SscsCaseData sscsCaseData, State state, EventType eventType) {
         CaseDetails<SscsCaseData> caseDetails = new CaseDetails<>(1L, "SSCS", state, sscsCaseData,
