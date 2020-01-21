19c8ce5ae58eb231f187e23dfec0971894e7d10e
srochani <sunil.rochani@hotmail.com>
2020-01-21 11:36:01 +0000

    SSCS-6809 Changes to support new PIP AE office added by DWP

--- src/main/java/uk/gov/hmcts/reform/sscs/domain/email/RoboticsEmailTemplate.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/domain/email/RoboticsEmailTemplate.java
@@ -32,7 +32,7 @@ public class RoboticsEmailTemplate {
                                boolean isPipAeTo) {
         return new Email(
                 from,
-                isPipAeTo ? pipAeTo : isScottish ? scottishTo : to,
+                isScottish ? scottishTo : isPipAeTo ? pipAeTo : to,
                 subject,
                 message,
                 attachments
