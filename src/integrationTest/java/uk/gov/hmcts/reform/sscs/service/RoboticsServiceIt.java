f425108abd15b62693c7c98b541930259b3efc26
jackmaloney <jack.maloney@transformuk.com>
2020-03-18 17:16:17 +0000

    SSCS-7316 Update closed DWP offices when robotics generated

--- src/integrationTest/java/uk/gov/hmcts/reform/sscs/service/RoboticsServiceIt.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sscs/service/RoboticsServiceIt.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.State.APPEAL_CREATED;
 
+import java.util.ArrayList;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
