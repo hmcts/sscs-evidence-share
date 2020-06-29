f9c4308df7db733da98576de63ad9191a23d09ab
Dan Hamilton <dan@emagineers.co.uk>
2020-06-29 16:55:48 +0100

    repeat test

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/EvidenceShareFunctionalTestThree.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/EvidenceShareFunctionalTestThree.java
@@ -1,18 +1,18 @@
 package uk.gov.hmcts.reform.sscs.functional;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.VALID_APPEAL_CREATED;
 
-import io.github.artsok.RepeatedIfExceptionsTest;
 import java.time.LocalDate;
 import java.util.List;
+import org.junit.Test;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocument;
 
-public class EvidenceShareFunctionalTest extends AbstractFunctionalTest {
+public class EvidenceShareFunctionalTestThree extends AbstractFunctionalTest {
 
-    public EvidenceShareFunctionalTest() {
+    public EvidenceShareFunctionalTestThree() {
         super();
     }
 
@@ -20,25 +20,25 @@ public class EvidenceShareFunctionalTest extends AbstractFunctionalTest {
         ccdCaseId = null;
     }
 
-    @RepeatedIfExceptionsTest(repeats = 3)
-    public void processAnAppealWithValidMrn_shouldGenerateADl6AndAddToCcdAndUpdateState() throws Exception {
+    @Test
+    public void processAnAppealWithLateMrn_shouldGenerateADl16AndAddToCcdAndUpdateState() throws Exception {
         Thread.sleep(5000);
         createCaseWithValidAppealState(VALID_APPEAL_CREATED);
 
         String json = getJson(VALID_APPEAL_CREATED.getCcdType());
         json = json.replace("CASE_ID_TO_BE_REPLACED", ccdCaseId);
-        json = json.replace("MRN_DATE_TO_BE_REPLACED", LocalDate.now().toString());
+        json = json.replace("MRN_DATE_TO_BE_REPLACED", LocalDate.now().minusDays(31).toString());
 
         simulateCcdCallback(json);
-
         SscsCaseDetails caseDetails = findCaseById(ccdCaseId);
         SscsCaseData caseData = caseDetails.getData();
 
         List<SscsDocument> docs = caseData.getSscsDocument();
+
+        assertNotNull("docs is not null", docs);
         assertEquals(1, docs.size());
-        assertEquals("dl6-" + ccdCaseId + ".pdf", docs.get(0).getValue().getDocumentFileName());
+        assertEquals("dl16-" + ccdCaseId + ".pdf", docs.get(0).getValue().getDocumentFileName());
         assertEquals("withDwp", caseDetails.getState());
         assertEquals(LocalDate.now().toString(), caseData.getDateSentToDwp());
-        assertEquals(LocalDate.now().toString(), caseData.getDateCaseSentToGaps());
     }
 }
---
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/EvidenceShareFunctionalTestTwo.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.sscs.functional;
+
+import static org.junit.Assert.*;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.VALID_APPEAL_CREATED;
+
+import io.github.artsok.RepeatedIfExceptionsTest;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+
+public class EvidenceShareFunctionalTestTwo extends AbstractFunctionalTest {
+
+    public EvidenceShareFunctionalTestTwo() {
+        super();
+    }
+
+    public void beforeEach() {
+        ccdCaseId = null;
+    }
+
+
+    @RepeatedIfExceptionsTest(repeats = 3)
+    public void processAnAppealWithNoValidMrnDate_shouldNoTBeSentToDwpAndShouldBeUpdatedToFlagError() throws Exception {
+        Thread.sleep(5000);
+        createCaseWithValidAppealState(VALID_APPEAL_CREATED);
+
+        String json = getJson(VALID_APPEAL_CREATED.getCcdType());
+        json = json.replace("CASE_ID_TO_BE_REPLACED", ccdCaseId);
+        json = json.replace("MRN_DATE_TO_BE_REPLACED", "");
+
+        simulateCcdCallback(json);
+        SscsCaseDetails caseDetails = findCaseById(ccdCaseId);
+
+        assertNull(caseDetails.getData().getSscsDocument());
+        assertEquals("validAppeal", caseDetails.getState());
+        assertEquals("failedSending", caseDetails.getData().getHmctsDwpState());
+    }
+}
