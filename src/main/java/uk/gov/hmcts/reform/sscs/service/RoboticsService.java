4d9356a6e78268a4ca1846fa6bf14f00756341d7
srochani <sunil.rochani@hotmail.com>
2020-07-02 12:59:33 +0100

    SSCS-7725: Robotics – Stop sending extra documents to the robot

--- src/main/java/uk/gov/hmcts/reform/sscs/service/RoboticsService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/RoboticsService.java
@@ -91,14 +91,10 @@ public class RoboticsService {
         log.info("Downloading additional evidence for robotics for case id {} ", caseDetails.getId());
         Map<SscsDocument, byte[]> additionalEvidence = downloadEvidence(caseData, Long.valueOf(caseData.getCcdCaseId()));
 
-        boolean isScottish = Optional.ofNullable(caseData.getRegionalProcessingCenter()).map(f -> equalsIgnoreCase(f.getName(), GLASGOW)).orElse(false);
-        boolean isPipAeTo = Optional.ofNullable(caseData.getAppeal().getMrnDetails()).map(m -> equalsIgnoreCase(m.getDwpIssuingOffice(), PIP_AE)).orElse(false);
-        boolean isDigitalCase = Optional.ofNullable(caseData.getCreatedInGapsFrom()).map(d -> equalsIgnoreCase(d, READY_TO_LIST.getId())).orElse(false);
-
         log.info("Downloading SSCS1 for robotics for case id {} ", caseDetails.getId());
         byte[] sscs1Form = downloadSscs1(caseData, Long.valueOf(caseData.getCcdCaseId()));
 
-        sendJsonByEmail(caseDetails.getId(), caseData.getAppeal(), roboticsJson, sscs1Form, additionalEvidence, isScottish, isPipAeTo, isDigitalCase);
+        sendJsonByEmail(caseDetails.getId(), caseData, roboticsJson, sscs1Form, additionalEvidence);
 
         log.info("Case {} Robotics JSON successfully sent for benefit type {}", caseDetails.getId(),
             caseData.getAppeal().getBenefitType().getCode());
@@ -225,8 +221,13 @@ public class RoboticsService {
         return roboticsAppeal;
     }
 
-    private void sendJsonByEmail(long caseId, Appeal appeal, JSONObject json, byte[] pdf, Map<SscsDocument, byte[]> additionalEvidence, boolean isScottish, boolean isPipAeTo, boolean isDigitalCase) {
-        Appellant appellant = appeal.getAppellant();
+    private void sendJsonByEmail(long caseId, SscsCaseData caseData, JSONObject json, byte[] pdf, Map<SscsDocument, byte[]> additionalEvidence) {
+
+        boolean isScottish = Optional.ofNullable(caseData.getRegionalProcessingCenter()).map(f -> equalsIgnoreCase(f.getName(), GLASGOW)).orElse(false);
+        boolean isPipAeTo = Optional.ofNullable(caseData.getAppeal().getMrnDetails()).map(m -> equalsIgnoreCase(m.getDwpIssuingOffice(), PIP_AE)).orElse(false);
+        boolean isDigitalCase = Optional.ofNullable(caseData.getCreatedInGapsFrom()).map(d -> equalsIgnoreCase(d, READY_TO_LIST.getId())).orElse(false);
+
+        Appellant appellant = caseData.getAppeal().getAppellant();
 
         String appellantUniqueId = emailHelper.generateUniqueEmailId(appellant);
 
@@ -250,7 +251,7 @@ public class RoboticsService {
         );
 
         log.info("Case {} robotics JSON email with subject '{}' sent successfully for benefit type {} isScottish {} isPipAe {}",
-            caseId, subject, appeal.getBenefitType().getCode(), isScottish, isPipAeTo);
+            caseId, subject, caseData.getAppeal().getBenefitType().getCode(), isScottish, isPipAeTo);
     }
 
     private String buildSubject(String appellantUniqueId, boolean isScottish) {
