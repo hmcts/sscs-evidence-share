d4b93fe5e299156ddbd3010db56d2c73e6a26ede
Dan Hamilton <dan@emagineers.co.uk>
2020-10-06 10:39:34 +0100

    SSCS-7810 Handle cover letter service failures to be unrecoverable
    This reverts commit a0f7d32c38d05b7da9de06268b6508fd15bc7fba.

--- src/main/java/uk/gov/hmcts/reform/sscs/service/CoverLetterService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/CoverLetterService.java
@@ -4,10 +4,13 @@ import static java.util.Objects.requireNonNull;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
+
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.docmosis.domain.DocumentHolder;
@@ -15,17 +18,29 @@ import uk.gov.hmcts.reform.sscs.docmosis.domain.Pdf;
 import uk.gov.hmcts.reform.sscs.docmosis.domain.Template;
 import uk.gov.hmcts.reform.sscs.docmosis.service.PdfGenerationService;
 import uk.gov.hmcts.reform.sscs.domain.FurtherEvidenceLetterType;
+import uk.gov.hmcts.reform.sscs.exception.UnableToContactThirdPartyException;
 import uk.gov.hmcts.reform.sscs.service.placeholders.FurtherEvidencePlaceholderService;
 
 @Service
 @Slf4j
 public class CoverLetterService {
 
-    @Autowired
+
     private FurtherEvidencePlaceholderService furtherEvidencePlaceholderService;
-    @Autowired
-    @Qualifier("docmosisPdfGenerationService")
+
     private PdfGenerationService pdfGenerationService;
+
+
+    private int maxRetryAttempts;
+
+    @Autowired
+    public CoverLetterService(FurtherEvidencePlaceholderService furtherEvidencePlaceholderService,
+                              @Qualifier("docmosisPdfGenerationService") PdfGenerationService pdfGenerationService,
+                              @Value("${send-letter.maxRetryAttempts:3}") int maxRetryAttempts) {
+        this.furtherEvidencePlaceholderService = furtherEvidencePlaceholderService;
+        this.pdfGenerationService = pdfGenerationService;
+        this.maxRetryAttempts = maxRetryAttempts;
+    }
 
     public void appendCoverLetter(byte[] coverLetterContent, List<Pdf> pdfsToBulkPrint, String pdfName) {
         requireNonNull(coverLetterContent, "coverLetter must not be null");
@@ -59,14 +74,32 @@ public class CoverLetterService {
 
         requireNonNull(caseData, "caseData must not be null");
 
-        byte[] coverLetterContent = pdfGenerationService.generatePdf(DocumentHolder.builder()
-            .template(new Template(templateName, hmctsDocName))
-            .placeholders(furtherEvidencePlaceholderService.populatePlaceholders(caseData, letterType))
-            .pdfArchiveMode(true)
-            .build());
-
-
-        printCoverLetterToPdfLocallyForDebuggingPurpose(coverLetterContent, letterType, hmctsDocName);
-        return coverLetterContent;
+        Map<String, Object> placeholders = furtherEvidencePlaceholderService.populatePlaceholders(caseData, letterType);
+        return generateCoverLetterRetry(letterType, templateName, hmctsDocName, placeholders, 1);
     }
+
+    public byte[] generateCoverLetterRetry(FurtherEvidenceLetterType letterType, String templateName,
+                                      String hmctsDocName, Map<String, Object> placeholders, int retries) {
+        try {
+
+            byte[] coverLetterContent = pdfGenerationService.generatePdf(DocumentHolder.builder()
+                .template(new Template(templateName, hmctsDocName))
+                .placeholders(placeholders)
+                .pdfArchiveMode(true)
+                .build());
+
+            printCoverLetterToPdfLocallyForDebuggingPurpose(coverLetterContent, letterType, hmctsDocName);
+
+            return coverLetterContent;
+
+        } catch (Exception e) {
+            if (retries < maxRetryAttempts) {
+                log.info("Retrying Cover Letter Service retry " + retries + " due to " + e.getMessage());
+                return generateCoverLetterRetry(letterType, templateName, hmctsDocName, placeholders, retries + 1);
+            } else {
+                throw new UnableToContactThirdPartyException("docmosis", e);
+            }
+        }
+    }
+
 }
