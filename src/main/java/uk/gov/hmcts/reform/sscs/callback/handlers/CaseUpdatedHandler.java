2c8f7b2ebca6a8d4a2c5fe1dd85910e4c12b4681
chrisdavecm <christopher.davidson@btinternet.com>
2020-10-09 12:30:23 +0100

    Added caseUpdated handler

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/CaseUpdatedHandler.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.sscs.callback.handlers;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.State.READY_TO_LIST;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.callback.CallbackHandler;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.DispatchPriority;
+import uk.gov.hmcts.reform.sscs.ccd.domain.BenefitType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+
+@Slf4j
+@Service
+public class CaseUpdatedHandler  implements CallbackHandler<SscsCaseData> {
+    private CcdService ccdService;
+    private IdamService idamService;
+
+    @Autowired
+    public CaseUpdatedHandler(CcdService ccdService, IdamService idamService) {
+        this.ccdService = ccdService;
+        this.idamService = idamService;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackType.equals(CallbackType.SUBMITTED)
+            && callback.getEvent() == EventType.CASE_UPDATED
+            && READY_TO_LIST.getId().equals(callback.getCaseDetails().getCaseData().getCreatedInGapsFrom());
+    }
+
+    @Override
+    public void handle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        if (!canHandle(callbackType, callback)) {
+            log.info("Cannot handle this event");
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        if (callback.getCaseDetails().getCaseData().getAppeal() == null
+            || callback.getCaseDetails().getCaseData().getAppeal().getBenefitType() == null) {
+            log.info("Cannot handle this event as no data");
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        SscsCaseData caseData = callback.getCaseDetails().getCaseData();
+
+        BenefitType benefitType = caseData.getAppeal().getBenefitType();
+        log.info("BenefitType" + benefitType);
+
+        if (StringUtils.equalsIgnoreCase(benefitType.getCode(), "uc")) {
+            if (caseData.getJointParty() != null && StringUtils.equalsIgnoreCase("Yes", caseData.getJointParty())) {
+                ccdService.updateCase(caseData, callback.getCaseDetails().getId(),
+                    EventType.JOINT_PARTY_ADDED.getCcdType(), "Joint party added",
+                    "description", idamService.getIdamTokens());
+                log.info("jointPartyAdded event updated");
+            }
+        }
+    }
+
+    @Override
+    public DispatchPriority getPriority() {
+        return DispatchPriority.LATEST;
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/callback/handlers/CaseUpdatedHandlerTest.java
@@ -0,0 +1,101 @@
+package uk.gov.hmcts.reform.sscs.callback.handlers;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static uk.gov.hmcts.reform.sscs.callback.handlers.HandlerHelper.buildTestCallbackForGivenData;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CASE_UPDATED;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.State.INTERLOCUTORY_REVIEW_STATE;
+
+@RunWith(JUnitParamsRunner.class)
+public class CaseUpdatedHandlerTest {
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+
+    @Mock
+    private IdamService idamService;
+    @Mock
+    private CcdService ccdService;
+
+    private CaseUpdatedHandler handler;
+
+    @Before
+    public void setUp() {
+        handler = new CaseUpdatedHandler(ccdService, idamService);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void givenCallbackIsNull_whenHandleIsCalled_shouldThrowException() {
+        handler.handle(CallbackType.SUBMITTED, null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @Parameters({"ABOUT_TO_START", "MID_EVENT", "ABOUT_TO_SUBMIT"})
+    public void givenCallbackIsNotSubmitted_willThrowAnException(CallbackType callbackType) {
+        handler.handle(callbackType,
+            buildTestCallbackForGivenData(null, INTERLOCUTORY_REVIEW_STATE, CASE_UPDATED));
+    }
+
+    @Test
+    public void givenACaseUpdatedWithJointParty_runJointPartyAddedEvent() {
+        final Callback<SscsCaseData> callback = buildTestCallbackForGivenData(
+            SscsCaseData.builder().ccdCaseId("1").createdInGapsFrom(State.READY_TO_LIST.getId()).jointParty("Yes")
+                .appeal(Appeal.builder()
+                .benefitType(BenefitType.builder().code("UC").build())
+                .build()).build(), INTERLOCUTORY_REVIEW_STATE, CASE_UPDATED);
+
+        when(idamService.getIdamTokens()).thenReturn(IdamTokens.builder().build());
+        when(ccdService.updateCase(any(), any(), any(), any(), any(), any())).thenReturn(SscsCaseDetails.builder().id(1L)
+            .data(callback.getCaseDetails().getCaseData()).build());
+
+        handler.handle(CallbackType.SUBMITTED, callback);
+
+        verify(idamService).getIdamTokens();
+        verify(ccdService).updateCase(eq(callback.getCaseDetails().getCaseData()),
+            eq(Long.valueOf(callback.getCaseDetails().getCaseData().getCcdCaseId())), eq(EventType.JOINT_PARTY_ADDED.getCcdType()), anyString(), anyString(), any());
+    }
+
+    @Test
+    public void givenACaseUpdatedWithJointPartyNo_dontRunJointPartyAddedEvent() {
+        final Callback<SscsCaseData> callback = buildTestCallbackForGivenData(
+            SscsCaseData.builder().ccdCaseId("1").createdInGapsFrom(State.READY_TO_LIST.getId()).jointParty("No")
+                .appeal(Appeal.builder()
+                    .benefitType(BenefitType.builder().code("UC").build())
+                    .build()).build(), INTERLOCUTORY_REVIEW_STATE, CASE_UPDATED);
+
+        handler.handle(CallbackType.SUBMITTED, callback);
+
+        verifyNoInteractions(idamService);
+        verifyNoInteractions(ccdService);
+    }
+
+    @Test
+    public void givenACaseUpdatedWithJointPartyNull_dontRunJointPartyAddedEvent() {
+        final Callback<SscsCaseData> callback = buildTestCallbackForGivenData(
+            SscsCaseData.builder().ccdCaseId("1").createdInGapsFrom(State.READY_TO_LIST.getId())
+                .appeal(Appeal.builder()
+                    .benefitType(BenefitType.builder().code("UC").build())
+                    .build()).build(), INTERLOCUTORY_REVIEW_STATE, CASE_UPDATED);
+
+        handler.handle(CallbackType.SUBMITTED, callback);
+
+        verifyNoInteractions(idamService);
+        verifyNoInteractions(ccdService);
+    }
+}
