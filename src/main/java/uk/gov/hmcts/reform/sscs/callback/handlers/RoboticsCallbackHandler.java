603f505d0f8908a1c655678b932560cbd662ce68
chrisdavecm <christopher.davidson@btinternet.com>
2020-10-21 16:43:51 +0100

    Revert "removed as it never gets checked for false"
    This reverts commit 93be5327fe6c9896253ea5997134ec17577689d3.

--- src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/RoboticsCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/RoboticsCallbackHandler.java
@@ -63,8 +63,8 @@ public class RoboticsCallbackHandler implements CallbackHandler<SscsCaseData> {
             || callback.getEvent() == INTERLOC_VALID_APPEAL
             || isValidStateForConfidentialityRequest(callback)
             || callback.getEvent() == EventType.SEND_TO_DWP)
-            || (callback.getEvent() == EventType.DWP_RAISE_EXCEPTION && ucEnabled)
-            && !callback.getCaseDetails().getCaseData().isTranslationWorkOutstanding())
+            && !callback.getCaseDetails().getCaseData().isTranslationWorkOutstanding()
+            || (callback.getEvent() == EventType.DWP_RAISE_EXCEPTION && ucEnabled))
             || callback.getEvent() == RESEND_CASE_TO_GAPS2;
     }
 
@@ -93,7 +93,7 @@ public class RoboticsCallbackHandler implements CallbackHandler<SscsCaseData> {
 
                 String ccdEventType = null;
                 if (callback.getEvent() == REVIEW_CONFIDENTIALITY_REQUEST
-                    || (callback.getEvent() == DWP_RAISE_EXCEPTION)) {
+                    || (callback.getEvent() == DWP_RAISE_EXCEPTION && ucEnabled)) {
                     ccdEventType = NOT_LISTABLE.getCcdType();
                 } else if (callback.getEvent() == EventType.READY_TO_LIST
                     || callback.getEvent() == RESEND_CASE_TO_GAPS2) {
@@ -131,7 +131,7 @@ public class RoboticsCallbackHandler implements CallbackHandler<SscsCaseData> {
 
         return callback.getEvent() == RESEND_CASE_TO_GAPS2
             || callback.getEvent() == REVIEW_CONFIDENTIALITY_REQUEST
-            || (callback.getEvent() == DWP_RAISE_EXCEPTION)
+            || (callback.getEvent() == DWP_RAISE_EXCEPTION && ucEnabled)
             || callback.getCaseDetails().getCaseData().getCreatedInGapsFrom() == null
             || StringUtils.equalsIgnoreCase(callback.getCaseDetails().getCaseData().getCreatedInGapsFrom(), callback.getCaseDetails().getState().getId()) ? true : false;
     }
