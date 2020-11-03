5698ad3b0e18816a38ca57e2f401c14b72d8133f
Tim Jacomb <tim.jacomb@hmcts.net>
2020-11-03 08:32:01 +0000

    DTSPO-115 Store send-grid API key in vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,3 +10,24 @@ data "azurerm_key_vault" "sscs_key_vault" {
   name                = "${local.azureVaultName}"
   resource_group_name = local.azureVaultName
 }
+
+data "azurerm_key_vault" "send_grid" {
+  provider = azurerm.send-grid
+
+  name                = var.env != "prod" ? "sendgridnonprod" : "sendgridprod"
+  resource_group_name = var.env != "prod" ? "SendGrid-nonprod" : "SendGrid-prod"
+}
+
+data "azurerm_key_vault_secret" "send_grid_api_key" {
+  provider = azurerm.send-grid
+
+
+  key_vault_id = data.azurerm_key_vault.send_grid.id
+  name         = "sscs-password"
+}
+
+resource "azurerm_key_vault_secret" "sendgrid_api_key" {
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+  name         = "sendgrid-api-key"
+  value        = data.azurerm_key_vault_secret.send_grid_api_key.value
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -11,24 +11,12 @@ variable "location" {
 variable "env" {
 }
 
-variable "ilbIp" {}
-
 variable "subscription" {}
-
-variable "capacity" {
-  default = "1"
-}
 
 variable "common_tags" {
   type = map(string)
 }
 
-variable "appinsights_instrumentation_key" {
-  description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-  default     = ""
-}
-
-variable "appinsights_location" {
-  default     = "West Europe"
-  description = "Location for Application Insights"
+variable "send_grid_subscription" {
+  default = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
 }
