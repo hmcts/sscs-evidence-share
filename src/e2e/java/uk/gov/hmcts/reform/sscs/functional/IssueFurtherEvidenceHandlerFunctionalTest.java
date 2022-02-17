990b104c6205b739d90dd078a71d09923fc3c5fa
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-02-17 09:56:21 +0000

    Add logging

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/IssueFurtherEvidenceHandlerFunctionalTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/IssueFurtherEvidenceHandlerFunctionalTest.java
@@ -39,7 +39,9 @@ public class IssueFurtherEvidenceHandlerFunctionalTest extends AbstractFunctiona
     public void givenIssueFurtherEventIsTriggeredWithExistingReasonableAdjustment_shouldNotBulkPrintEvidenceAndCoverLetterAndSetEvidenceIssuedToYes()
         throws IOException {
         String issueFurtherEvidenceCallback = createTestData(ISSUE_FURTHER_EVIDENCE.getCcdType() + "ExistingReasonableAdjustment");
+        System.out.println("[givenIssueFurtherEventIsTriggeredWithExistingReasonableAdjustment_shouldNotBulkPrintEvidenceAndCoverLetterAndSetEvidenceIssuedToYes] issueFurtherEvidenceCallback :  " + issueFurtherEvidenceCallback);
         simulateCcdCallback(issueFurtherEvidenceCallback);
+
         verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised();
     }
 
@@ -82,6 +84,17 @@ public class IssueFurtherEvidenceHandlerFunctionalTest extends AbstractFunctiona
         SscsCaseDetails caseDetails = findCaseById(ccdCaseId);
         SscsCaseData caseData = caseDetails.getData();
         List<SscsDocument> docs = caseData.getSscsDocument();
+
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.size() :  " + docs.size());
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.get(0) :  " + docs.get(0).getValue().toString());
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.get(1) :  " + docs.get(1).getValue().toString());
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.get(2) :  " + docs.get(2).getValue().toString());
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.get(3) :  " + docs.get(3).getValue().toString());
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.get(0).getEvidenceIssued( :  " + docs.get(0).getValue().getEvidenceIssued());
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.get(1).getEvidenceIssued( :  " + docs.get(1).getValue().getEvidenceIssued());
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.get(2).getEvidenceIssued( :  " + docs.get(2).getValue().getEvidenceIssued());
+        System.out.println("[verifyEvidenceIssuedAndExistingReasonableAdjustmentRaised] docs.get(3).getEvidenceIssued( :  " + docs.get(3).getValue().getEvidenceIssued());
+
 
         assertNull(docs.get(0).getValue().getEvidenceIssued());
         assertEquals("Yes", docs.get(1).getValue().getEvidenceIssued());
