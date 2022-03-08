407aa1d310844af820f7db32f936a16cef778f62
Chris Davidson <christopher.davidson@btinternet.com>
2022-03-08 09:27:20 +0000

    sscs 10068 supp data (#1236)
    * sscs 10068 supp data
    * sscs 10068 update common

---
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/ManualCaseCreatedFunctionalTest.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.sscs.functional;
+
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.*;
+
+import org.junit.Test;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+
+public class ManualCaseCreatedFunctionalTest extends AbstractFunctionalTest {
+    @Test
+    public void processANonDigitalAppealWithValidMrn_shouldGenerateADl6AndAddToCcdAndUpdateState() throws Exception {
+
+        SscsCaseDetails caseDetails = createNonDigitalCaseWithEvent(VALID_APPEAL_CREATED);
+
+        Thread.sleep(3000);
+
+        updateCaseEvent(SEND_TO_DWP_OFFLINE, caseDetails);
+    }
+}
