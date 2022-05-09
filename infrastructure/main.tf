3fac4d040d8742a84774339e3834d673cdc42960
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-05-09 14:05:10 +0100

    Tagging added

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,8 +7,12 @@ locals {
 }
 
 data "azurerm_key_vault" "sscs_key_vault" {
-  name                = "${local.azureVaultName}"
+  name                = local.azureVaultName
   resource_group_name = local.azureVaultName
+
+  tags = (merge(var.common_tags,
+    map("lastUpdated", "${timestamp()}")
+  ))
 }
 
 data "azurerm_key_vault" "send_grid" {
@@ -30,4 +34,8 @@ resource "azurerm_key_vault_secret" "sendgrid_api_key" {
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
   name         = "sendgrid-api-key"
   value        = data.azurerm_key_vault_secret.send_grid_api_key.value
+
+  tags = (merge(var.common_tags,
+    map("lastUpdated", "${timestamp()}")
+  ))
 }
