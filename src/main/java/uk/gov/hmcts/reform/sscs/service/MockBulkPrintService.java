fe44e781616c9d7cc2fc43e2ceb1536c6885affe
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-10-05 14:09:28 +0100

    SSCS-10956 Refactoring CcdNotificationsPdfService

--- src/main/java/uk/gov/hmcts/reform/sscs/service/MockBulkPrintService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/MockBulkPrintService.java
@@ -3,42 +3,37 @@ package uk.gov.hmcts.reform.sscs.service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.docmosis.domain.Pdf;
 import uk.gov.hmcts.reform.sscs.domain.FurtherEvidenceLetterType;
 
-
 @Service("bulkPrintService")
+@Slf4j
 @ConditionalOnProperty(prefix = "send-letter", name = "url", havingValue = "false")
+@RequiredArgsConstructor
 public class MockBulkPrintService implements PrintService {
-    private static final Logger logger = LoggerFactory.getLogger(MockBulkPrintService.class);
-
-    private final CcdNotificationsPdfService ccdNotificationsPdfService;
+    public static final String MOCK_UUID = "abc123ca-c336-11e9-9cb5-123456789abc";
 
     private final BulkPrintServiceHelper bulkPrintServiceHelper;
 
-    public MockBulkPrintService(CcdNotificationsPdfService ccdNotificationsPdfService,
-                                BulkPrintServiceHelper bulkPrintServiceHelper) {
-        this.ccdNotificationsPdfService = ccdNotificationsPdfService;
-        this.bulkPrintServiceHelper = bulkPrintServiceHelper;
-    }
-
     public Optional<UUID> sendToBulkPrint(List<Pdf> pdfs, SscsCaseData sscsCaseData) {
-        logger.info("No bulk print operation needs to be performed as 'Bulk print url' is switched off.");
-        return Optional.of(UUID.fromString("abc123ca-c336-11e9-9cb5-123456789abc"));
+        log.info("No bulk print operation needs to be performed as 'Bulk print url' is switched off.");
+        return Optional.of(UUID.fromString(MOCK_UUID));
     }
 
 
-    public Optional<UUID> sendToBulkPrint(List<Pdf> pdfs, SscsCaseData sscsCaseData, FurtherEvidenceLetterType letterType, EventType event) {
+    public Optional<UUID> sendToBulkPrint(List<Pdf> pdfs, SscsCaseData sscsCaseData,
+                                          FurtherEvidenceLetterType letterType, EventType event) {
         if (bulkPrintServiceHelper.sendForReasonableAdjustment(sscsCaseData, letterType)) {
-            logger.info("Sending to bulk print service {} reasonable adjustments enabled {}", sscsCaseData.getCcdCaseId());
+            log.info("Sending to bulk print service reasonable adjustments enabled {}", sscsCaseData.getCcdCaseId());
             bulkPrintServiceHelper.saveAsReasonableAdjustment(sscsCaseData, pdfs, letterType, event);
         } else {
-            logger.info("No bulk print operation needs to be performed as 'Bulk print url' is switched off.");
+            log.info("No bulk print operation needs to be performed as 'Bulk print url' is switched off.");
         }
 
         return Optional.empty();
