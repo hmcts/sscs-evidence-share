99c9a32f49e96a08fd9c855b5f1c228bbc958848
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2022-10-27 14:38:17 +0100

    SSCS-11050 temp unfinished change to remove usage of springfox

--- src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
@@ -1,17 +1,15 @@
 package uk.gov.hmcts.reform.sscs.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.sscs.SscsEvidenceShareApplication;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
+    /*
 
     @Bean
     public Docket api() {
@@ -21,6 +19,20 @@ public class SwaggerConfiguration {
             .apis(RequestHandlerSelectors.basePackage(SscsEvidenceShareApplication.class.getPackage().getName() + ".controllers"))
             .paths(PathSelectors.any())
             .build();
+    }
+    */
+
+    // This is boilerplate and requires implementing real values of the above commented code
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+            .info(new Info().title("Blah")
+            .description("blah")
+            .version("1.2.3")
+            .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+            .externalDocs(new ExternalDocumentation()
+                .description("Deocumnetanetion")
+                .url("www.web.io"));
     }
 
 }
