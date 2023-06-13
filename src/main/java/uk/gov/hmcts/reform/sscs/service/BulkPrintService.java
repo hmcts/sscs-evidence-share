8a6e929e92e207257cff54f220a94e7e25de58c4
hajam09 <hajam09@yahoo.com>
2023-06-13 09:02:31 +0100

    update

--- src/main/java/uk/gov/hmcts/reform/sscs/service/BulkPrintService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/BulkPrintService.java
@@ -30,6 +30,7 @@ public class BulkPrintService implements PrintService {
     private static final String CASE_IDENTIFIER = "caseIdentifier";
     private static final String LETTER_TYPE_KEY = "letterType";
     private static final String APPELLANT_NAME = "appellantName";
+    private static final String RECIPIENTS = "recipients";
 
     private final SendLetterApi sendLetterApi;
     private final IdamService idamService;
@@ -118,7 +119,7 @@ public class BulkPrintService implements PrintService {
         additionalData.put(APPELLANT_NAME, sscsCaseData.getAppeal().getAppellant().getName().getFullNameNoTitle());
         List<String> parties = new ArrayList<>();
         parties.add("test");
-        additionalData.put("key", parties.toArray());
+        additionalData.put(RECIPIENTS, parties.toArray());
         return additionalData;
     }
 }
