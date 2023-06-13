32a147ef760868c150488d8903d79c176e04f7ad
hajam09 <hajam09@yahoo.com>
2023-06-13 12:43:20 +0100

    update

--- src/main/java/uk/gov/hmcts/reform/sscs/service/BulkPrintService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/BulkPrintService.java
@@ -117,7 +117,7 @@ public class BulkPrintService implements PrintService {
         additionalData.put(LETTER_TYPE_KEY, "sscs-data-pack");
         additionalData.put(CASE_IDENTIFIER, sscsCaseData.getCcdCaseId());
         additionalData.put(APPELLANT_NAME, sscsCaseData.getAppeal().getAppellant().getName().getFullNameNoTitle());
-        additionalData.put(RECIPIENTS, "Barry Allen, Oliver Queen");
+        additionalData.put(RECIPIENTS, "Barry Allen");
         return additionalData;
     }
 }
