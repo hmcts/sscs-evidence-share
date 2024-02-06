28a56e74e5a7ebe8a9867e56b3608ebe9d112909
NHAL22 <113360585+NHAL22@users.noreply.github.com>
2024-02-06 12:59:05 +0000

    add in name

--- src/main/java/uk/gov/hmcts/reform/sscs/service/placeholders/SorPlaceholderService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/placeholders/SorPlaceholderService.java
@@ -41,7 +41,7 @@ public class SorPlaceholderService {
         placeholders.putAll(PlaceholderUtility.getAddressPlaceHolders(address));
 
         if (name != null) {
-            placeholders.put(NAME, name);
+            placeholders.put(ADDRESS_NAME, name);
         }
         placeholders.put(HMCTS2, HMCTS_IMG);
         placeholders.put(APPEAL_REF, caseData.getCcdCaseId());
