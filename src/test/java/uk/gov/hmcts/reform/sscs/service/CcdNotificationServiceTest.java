3d55e96a8b643c10b3200edb170aad55d4a1d6dc
NHAL22 <113360585+NHAL22@users.noreply.github.com>
2024-02-12 16:37:09 +0000

    add To field in correspondence

--- src/test/java/uk/gov/hmcts/reform/sscs/service/CcdNotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/CcdNotificationServiceTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscs.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Correspondence;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,11 +24,24 @@ class CcdNotificationServiceTest {
         CcdNotificationService ccdNotificationService = new CcdNotificationService(ccdNotificationsPdfService);
         byte[] letter = new byte[1];
         var event = EventType.ISSUE_GENERIC_LETTER;
+        var recipient = "Test";
         Long caseId = 0L;
         String senderType = "Bulk Print";
 
-        ccdNotificationService.storeNotificationLetterIntoCcd(event, letter, caseId);
+        ccdNotificationService.storeNotificationLetterIntoCcd(event, letter, caseId, recipient);
         verify(ccdNotificationsPdfService, times(1))
             .mergeLetterCorrespondenceIntoCcd(eq(letter), eq(caseId), any(), eq(senderType));
+    }
+
+    @Test
+    void verifyToFieldOnCorrespondenceForSorLetter() {
+        CcdNotificationService ccdNotificationService = new CcdNotificationService(ccdNotificationsPdfService);
+        EventType eventType = EventType.SOR_WRITE;
+        String recipient = "Test name";
+
+        Correspondence correspondence = ccdNotificationService.buildCorrespondence(eventType, recipient);
+
+        assertEquals(recipient, correspondence.getValue().getTo());
+        assertEquals(eventType.getCcdType(), correspondence.getValue().getEventType());
     }
 }
