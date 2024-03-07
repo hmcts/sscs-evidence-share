f945624c463ae244a7f1bbffdcd36cfbdf576e5b
andrewmcmahon <andrew.mcmahon@solirius.com>
2024-03-07 12:11:04 +0000

    Test for appelant name

--- src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/IssueFurtherEvidenceHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/IssueFurtherEvidenceHandler.java
@@ -71,6 +71,20 @@ public class IssueFurtherEvidenceHandler implements CallbackHandler<SscsCaseData
             updateCcdCaseService.updateCaseV2(caseIdLong, EventType.UPDATE_CASE_ONLY.getCcdType(),
                 idamService.getIdamTokens(), caseData -> {
                     log.info("HmctsDwpState is {} at pre mutator", caseData.getHmctsDwpState());
+                    if (caseData.getAppeal() != null) {
+                        if (caseData.getAppeal().getAppellant() != null) {
+                            if (caseData.getAppeal().getAppellant().getName() != null) {
+                                log.info("Appelant.Name is {}", caseData.getAppeal().getAppellant().getName());
+                            } else {
+                                log.info("Appelant.Name is null");
+                            }
+                        } else {
+                            log.info("Appelant is null");
+                        }
+                    } else {
+                        log.info("Appeal is null");
+                    }
+
                     issueFurtherEvidence(caseData);
                     String description = postIssueFurtherEvidenceTasks(caseData);
                     log.info("HmctsDwpState is {} at post mutator", caseData.getHmctsDwpState());
